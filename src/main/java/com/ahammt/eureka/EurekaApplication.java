18aaa3833779cec98c53dcc044f84b46d18cda71
huaxin <hua.xin@jyall.com>
2018-08-17 17:07:25 +0800

    初始化项目

--- src/main/java/com/ahammt/eureka/EurekaApplication.java
+++ src/main/java/com/ahammt/eureka/EurekaApplication.java
@@ -6,9 +6,9 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @EnableEurekaServer
 @SpringBootApplication
-public class IsEurekaApplication {
+public class EurekaApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(IsEurekaApplication.class, args);
+        SpringApplication.run(EurekaApplication.class, args);
     }
 }
